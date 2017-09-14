56c03d7bb4c4a6c5cf61c2e3c9632f83769be8be
chenglong <250407778@qq.com>
2017-09-14 13:48:09 +0800

    更改成单页面应用

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ const app = dva({
 // app.use({});
 
 // 3. Model
-app.model(require('../models/login'))
+app.model(require('./models/login'))
 
 app.model(require('./models/chatting'))
 
 app.model(require('./models/users'))
 
 // 4. Router
-app.router(require('../router'))
+app.router(require('./router'))
 
 // 5. Start
 app.start('#root')
